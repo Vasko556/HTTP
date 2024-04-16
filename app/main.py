a6ef1562b4cb4493a84361218ca1531366176599
Vasko556 <vasil.aleksandrov556@gmail.com>
2024-04-17 01:53:32 +0300

    test

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from threading import Thread
 #conn, _= server_socket.accept()
 
 
-def main():
+def main(conn):
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, _= server_socket.accept()
