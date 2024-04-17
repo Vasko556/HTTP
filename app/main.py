a81e3678d10baf153ccb8ff31b3b98653f9fa00e
Vasko556 <vasil.aleksandrov556@gmail.com>
2024-04-18 01:17:07 +0300

    Split

--- app/main.py
+++ app/main.py
@@ -1,47 +1,33 @@
 import socket
-import threading 
 
 
-server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-conn, _= server_socket.accept()
+def main():
 
-def main(conn):
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    conn, _= server_socket.accept()
 
-    while True:
-            
-            with conn:
-                msg = conn.recv(1024)
-                msg1 = msg[10:-90]
-                msg2 = len(msg1)
-                msg3 = msg1.decode("utf-8")
-                user_agent = ""
-                user_agent_split = ""
-                if msg[0:6] == b'GET / ':
-                    data = "HTTP/1.1 200 OK\r\n\r\n"
-                elif msg[0:10] == b'GET /echo/':
-                    data = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: " + str(msg2) + "\r\n\r\n" + str(msg3)
-                elif msg[0:15].decode("utf-8") == 'GET /user-agent':
-                    user_agent = msg.decode("utf-8").split("Agent: ", 1)[-1]
-                    user_agent_split = user_agent.split("\r\nAccept-", 1)[0]
-                    data = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: " + str(len(user_agent_split)) + "\r\n\r\n" + str(user_agent_split)
-
-                else:
-                    data = "HTTP/1.1 404 Not found\r\n\r\n"
-                #print(msg1)
-                #print(user_agent_split)
-                #print(data)
-                conn.send(data.encode("utf-8"))
-
-
-
-#if __name__ == "__main__":
-
-t = threading.Thread(target=main, args=(conn,))
-t.start()
-conn2, _= server_socket.accept()
-t2 = threading.Thread(target=main, args=(conn2,))
-t2.start()
-conn3, _= server_socket.accept()
-t3 = threading.Thread(target=main, args=(conn3,))
-t3.start()
-#main(conn,)
+    with conn:
+        msg = conn.recv(1024).decode()
+        substring = "GET / "
+        substring2 = "/echo/"
+        substring3 = "/user-agent"
+       
+        if msg.__contains__ (substring):
+            data = "HTTP/1.1 200 OK\r\n\r\n"
+        
+        elif msg.__contains__(substring2):
+            content2 = msg.split("/echo/", 1)[-1]       #from where to cut
+            info = content2.split(" HTTP/1.1", 1)[0]    #until where to cut 
+            data = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: "+ str(len(info)) + "\r\n\r\n" + str(info)
+        
+        elif msg.__contains__(substring3):
+            content2 = msg.split("Agent: ", 1)[-1]
+            info = content2.split("\r\nAccept-Encoding:", 1)[0]
+            data = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: "+ str(len(info)) + "\r\n\r\n" + str(info)
+        
+        else: 
+            data = "HTTP/1.1 404 Not found\r\n\r\n"
+       
+        conn.send(data.encode("utf-8"))
+if __name__ == "__main__":
+    main()
